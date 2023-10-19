69748d7e2317d5283141d615ade0e8b4a0fc61b3
Kaung Khant-ian <106058641+k4un62899@users.noreply.github.com>
2023-10-19 13:36:32 +0700

    First Deployment

---
+++ Portfolio/js/script.js
@@ -0,0 +1,23 @@
+/* Responsive Nav */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function openSkill(evt, skillName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(skillName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
